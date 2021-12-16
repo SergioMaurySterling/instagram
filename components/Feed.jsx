428e96eadb63ce253eba05a3b61b2129acfa124e
SergioMaurySterling <52435269+SergioMaurySterling@users.noreply.github.com>
2021-12-16 09:44:39 -0500

    Feed structure jsx

---
+++ components/Feed.jsx
@@ -0,0 +1,19 @@
+export default function Feed() {
+  return (
+    <main>
+
+      {/* Section Left */}
+      <section>
+        {/* Stories */}
+        {/* Posts */}
+      </section>
+
+      {/* Section right */}
+      <section>
+        {/* Mini profile */}
+        {/* Suggestion */}
+      </section>
+
+    </main>
+  )
+}
