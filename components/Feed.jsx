b671ff1187840addc32d18aaa3549c836802d6fe
SergioMaurySterling <52435269+SergioMaurySterling@users.noreply.github.com>
2021-12-16 21:48:22 -0500

    Feed centering

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -3,7 +3,7 @@ import Stories from "./Stories"
 export default function Feed() {
   return (
     <main className="grid gird-cols-1 md:grid-cols-2
-    md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl max-auto">
+    md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto">
 
       {/* Section Left */}
       <section className='col-span-2'>
