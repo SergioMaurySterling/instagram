f834980d3c4f4e14b10df7e690d9bb450ca100cd
SergioMaurySterling <52435269+SergioMaurySterling@users.noreply.github.com>
2022-01-07 20:49:03 -0500

    Post UI

--- components/Post.jsx
+++ components/Post.jsx
@@ -3,13 +3,14 @@ import {
   DotsHorizontalIcon,
   EmojiHappyIcon,
   HeartIcon,
+  ChatIcon,
   PaperAirplaneIcon
 } from '@heroicons/react/outline'
 import { HeartIcon as HeartIconFilled } from '@heroicons/react/solid'
 
 export default function Post({ id, username, userImg, img, caption }) {
   return (
-    <div>
+    <div className='bg-white my-7 border rounded-sm'>
       {/* Header */}
       <div className="flex items-center p-5">
         <img className='h-12 w-12
@@ -18,14 +19,37 @@ export default function Post({ id, username, userImg, img, caption }) {
         <DotsHorizontalIcon className='h-5 w-5' />
       </div>
       {/* Img */}
+      <img className='object-cover w-full h-auto' src={img} alt='picture' />
 
       {/* Buttons */}
+      <div className='flex justify-between px-4 pt-4'>
+        <div className='flex space-x-4'>
+          <HeartIcon className='btn'/>
+          <ChatIcon className='btn'/>
+          <PaperAirplaneIcon className='btn'/>
+        </div>
+        <BookmarkIcon className='btn'/>
+      </div>
 
       {/* Caption */}
+      <div>
+        <p className='p-5 truncate'>
+          <span className='font-bold mr-1'>{username}</span>
+          {caption}
+        </p>
+      </div>
 
       {/* Comments */}
 
       {/* Input box */}
+      <form className='flex items-center p-4'>
+        <EmojiHappyIcon className='btn' />
+        <input className='border-none flex-1
+        focus:ring-0 outline-none' type='text'
+        placeholder='Add a comment...' />
+        <buttonc className='font-semibold
+        text-blue-400 cursor-pointer '>Post</buttonc>
+      </form>
     </div>
   )
 }
