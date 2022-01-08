6bfad5b9cd601006c169d5c7289386dfc43a1f04
SergioMaurySterling <52435269+SergioMaurySterling@users.noreply.github.com>
2022-01-07 21:38:32 -0500

    suggestion data

--- components/Suggestions.jsx
+++ components/Suggestions.jsx
@@ -1,6 +1,46 @@
+import { useEffect, useState } from 'react'
+import faker from "faker"
+
 export default function Suggestions() {
+
+  const [suggestions, setSuggestions] = useState([])
+
+  useEffect(() => {
+    const suggestions = [...Array(5)].map((_,i) => (
+      {
+        ...faker.helpers.contextualCard(),
+        id: i
+      }
+    ))
+    setSuggestions(suggestions)
+  },[])
+
   return (
-    <div>
+    <div className='mt-4 ml-10'>
+      <div className='flex justify-between
+      text-sm mb-5'>
+        <h3 className='text-sm
+        font-bold text-gray-400'>Suggestions for you</h3>
+        <button className='text-gray-600
+        font-semibold'>See all</button>
+      </div>
+      {
+         suggestions.map(profile => (
+           <div key={profile.id}
+           className='flex items-center
+           justify-between mt-3'>
+            <img
+            className='w-10 h-10 rounded-full
+            border p-[2px]'
+            src='https://firebasestorage.googleapis.com/v0/b/petti-2d60d.appspot.com/o/images%2F4Pe0SOSeY1RRfkcJceD8OYWK4vc2?alt=media&token=d0347cb6-7474-4544-a8ce-9809e58a8363' alt='Profile picture' />
+            <div className='flex-1 ml-4'>
+              <h2 className='font-semibold text-sm'>{profile.username}</h2>
+              <h3 className='text-xs text-gray-400'>Works at {profile.company.name}</h3>
+            </div>
+            <button className='text-blue-400 text-xs font-semibold'>Follow</button>
+           </div>
+         ))
+      }
     </div>
   )
 }
