2c09f23220c6e4284d5571758228732f32631954
SergioMaurySterling <52435269+SergioMaurySterling@users.noreply.github.com>
2022-01-09 12:25:13 -0500

    logrocket

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import '../styles/globals.css'
+import LogRocket from 'logrocket';
 import { SessionProvider } from 'next-auth/react'
 import { RecoilRoot } from 'recoil'
+
+LogRocket.init('la2cir/instasams');
 
 function MyApp({ Component, pageProps: {session, ...pageProps} }) {
   return (
